76fe8353581ebf415645691ae1ee2deef13280d8
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-06-04 21:04:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,7 +136,7 @@ end
 
 def team_colors(team)
   game_hash.each do |location, team_info|
-    team_info[:colors] if team_info[:team_name] == team
+    return team_info[:colors] if team_info[:team_name] == team
   end
 end
 
